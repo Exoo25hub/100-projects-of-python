babe22fbf99f4852520fc026e56d90cedb73d50f
Mahdi Ali <Exoo25pro@gmail.com>
2025-02-08 21:52:24 +0500

    Create guess_the_number_day2.py

---
+++ guess_the_number_day2.py
@@ -0,0 +1,29 @@
+import time
+import random
+from pyfiglet import figlet_format
+
+print(figlet_format("Guess The Number"))
+
+while True:
+    computer = random.randint(1, 100)  # Generate a random number each round
+    
+    while True:
+        user_input = input("I have chosen a number from 1 to 100. Guess it: ")
+        
+        if not user_input.isdigit():
+            print("Please enter a valid number.")
+            continue
+        
+        user_guess = int(user_input)
+        
+        if user_guess == computer:
+            print(f"Right! I guessed {computer}. You got it!")
+            
+        else:
+            print(f"Wrong! Try again. i guessed {computer")
+    
+    cont = input("Do you want to continue? (yes/no): ").strip().lower()
+    if cont != "yes":
+        print("Thanks for playing!")
+        time.sleep(3)
+        break
