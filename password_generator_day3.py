0ddf721c11bceceb6b2aa5407f37acd566e72f79
Mahdi Ali <Exoo25pro@gmail.com>
2025-02-09 14:31:47 +0500

    Create password_generator_day3.py

---
+++ password_generator_day3.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+lentgh = input("password lentgh: ")
+all_chars = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^',
+    '_', '`', '{', '|', '}', '~', ' ', '\t', '\n', '\r', '\x0b', '\x0c'
+]
+password = ""
+for i in range(lentgh):
+	password += random.choice(all_chars)
+print(password)
+	
